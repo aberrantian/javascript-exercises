8f12f6337e84d9354e0f84c3de8751528f0b43f5
aberrantian <aberrantian@gmail.com>
2022-10-20 00:09:24 +0100

    complete reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let newString = '';
+    
+    for (index = stringLength -1; index > -1; index--) {
+        newString += string[index];
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
