ed6e4b5b4ed71c937207b6323135356f8ff6fe18
aberrantian <aberrantian@gmail.com>
2022-10-22 01:47:20 +0100

    complete removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rem) {
+	let newArray = [];
 
+	for (const arrayItem of array) {
+		let isInRem = false;
+		
+		for (const remItem of rem) {
+			if (arrayItem === remItem) {
+				isInRem = true;
+				break;
+			} else {
+				continue;
+			};
+		};
+
+		if (isInRem) {
+			continue;
+		} else {
+			newArray.push(arrayItem);
+		};
+	};
+
+	return newArray;
 };
 
 // Do not edit below this line
