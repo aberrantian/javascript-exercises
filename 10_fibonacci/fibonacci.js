0aa6c17b66a7eb36aa7306beb3bb84a5dcab32d6
aberrantian <aberrantian@gmail.com>
2022-10-30 03:05:55 +0000

    complete fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return 'OOPS';
+    } else {
+        const fib = [1, 1];
+        for (let i = 0; i +1 < index; i++) {
+            fib.push(fib[i] + fib[i+1]);
+        };
+        return fib[index -1];
+    };
 };
 
 // Do not edit below this line
