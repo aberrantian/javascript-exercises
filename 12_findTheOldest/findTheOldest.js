69dced63f9d56e8f14ece2b66d87177b3aeef9c1
aberrantian <aberrantian@gmail.com>
2022-10-30 03:46:59 +0000

    complete findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const today = new Date();
+    const year = today.getFullYear();
 
+    
+    let calcAge = function (person) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = year;
+        };
+        return person.yearOfDeath - person.yearOfBirth;
+    };
+    
+    let oldest = array[0];
+    for (let item of array) {
+        if (calcAge(item) > calcAge(oldest)) {
+            oldest = item;
+        };
+    };
+
+    return oldest;
 };
 
 // Do not edit below this line
