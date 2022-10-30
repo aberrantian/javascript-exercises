07ba3c251a727beef72fcbcd7679a56790a7cd17
aberrantian <aberrantian@gmail.com>
2022-10-30 02:50:57 +0000

    complete palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let inputString = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == ' ' ||
+            string[i] == ',' ||
+            string[i] == '.' ||
+            string[i] == '!') {
+            continue;
+        } else {
+            inputString += string[i];
+        };
+    };
 
+    let outputString = '';
+    for (let i = inputString.length -1; i >= 0; i--) {
+        outputString += inputString[i];
+    };
+    return outputString.toLocaleLowerCase() === inputString.toLocaleLowerCase();
 };
 
 // Do not edit below this line
