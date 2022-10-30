d8aedc137d44a00767f947ea36937a11c4cd1f26
aberrantian <aberrantian@gmail.com>
2022-10-30 02:13:16 +0000

    complete calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let item of array) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1] === undefined) {
+      return total;
+    };
 
+    total = total * array[i + 1];
+  };
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    let array = [];
+    for (let i = 1; i <= n; i++) {
+      array.push(i);
+    };
+  
+    return multiply(array);
+  };
 };
 
 // Do not edit below this line
